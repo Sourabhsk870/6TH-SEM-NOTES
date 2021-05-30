40c5bcdb96ba2ad6a009549d21accd9930beedc3
Sourabhsk870 <sourabhsk112@gmail.com>
2021-05-30 22:00:21 +0530

    Added Assignment 2 Answer

---
+++ 18CSL66/ASSIGNMENT/ASSIGNMENT - 2/ANSWER/demo.c
@@ -0,0 +1,11 @@
+// This is a comment line 
+// This is a comment line 
+#include <stdio.h>
+int main (){
+	// This is a comment line to print hello world 
+
+	printf("Program 6A\n");
+	
+	// This is a comment line 
+	// This is a comment line 
+}
---
+++ 18CSL66/ASSIGNMENT/ASSIGNMENT - 2/ANSWER/demo1.c
@@ -0,0 +1,11 @@
+
+
+#include <stdio.h>
+int main (){
+	
+
+	printf("Program 6A\n");
+	
+	
+	
+}
